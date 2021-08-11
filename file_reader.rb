e2980581ffcd4a4c3525f1aed1bf3347ad31e219
Nya Furtick <nya@candlescience.com>
2021-08-11 10:26:46 -0400

    Create FileReader class
    FileReader class accepts a file and parses it based off the "eoc" delineater seperating it into a canvas and acolor map of yarns.

---
+++ file_reader.rb
@@ -0,0 +1,27 @@
+class FileReader
+  END_OF_CANVAS = "eoc"
+
+  attr_reader :file_name
+
+  def initialize(file_name)
+    @file_name = file_name
+  end
+
+  def source_file
+    File.readlines(file_name, chomp: true)
+  end
+
+  def eoc
+    source_file.index(END_OF_CANVAS)
+  end
+
+  def canvas
+    canvas = source_file.slice(0, eoc)
+    canvas.map &:split
+  end
+
+  def yarns
+    color_map = source_file.slice((eoc + 1)...)
+    color_map.map { |value| value.split(":") }.to_h
+  end
+end
