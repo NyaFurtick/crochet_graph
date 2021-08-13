fe48ec478b15043cf1df4795c104e80e274d9be7
Nya Furtick <nya@candlescience.com>
2021-08-13 15:04:58 -0400

    Allow for which graph to render through parameters

--- app.rb
+++ app.rb
@@ -2,11 +2,19 @@ require 'sinatra'
 require './file_reader'
 
 get '/' do
-  'Welcome to graphgan!'
+  erb :index
 end
 
 get '/graph' do
-  @graph = FileReader.new("canvas.txt").graphgan
+  erb :graph_index
+end
+
+get '/graph/:file_name' do
+  file_name = params['file_name'].concat '.txt'
+  @graph = FileReader.new(file_name).graphgan
 
   erb :graph
+
+rescue Errno::ENOENT
+  'file does not exist'
 end
