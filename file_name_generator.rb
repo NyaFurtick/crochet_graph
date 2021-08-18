f04ebadfb53347e964940081d615880a091ffd47
Nya Furtick <nya@candlescience.com>
2021-08-18 16:13:45 -0400

    Add FileNameGenerator class
    Handles changing the file extension of the file uploaded by user.

---
+++ file_name_generator.rb
@@ -0,0 +1,13 @@
+class FileNameGenerator
+  attr_reader :file_name
+
+  def initialize(file_name)
+    @file_name = file_name
+  end
+
+  def gif
+    # removes file extension
+    no_ext = File.basename(file_name, File.extname(file_name))
+    file_name = no_ext.concat('.gif')
+  end
+end
